6c223f0fed4807905a5758ac16aad2635e002bee
galanm <galan.mateusz@gmail.com>
2014-04-02 00:54:11 +0200

    Added lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,6 +7,12 @@ class LessonsController < ApplicationController
 
   def create
     @lesson = @course.lessons.create(lesson_params)
+
+    if @lesson.save
+      redirect_to courses_path, notice: "You have successfully added a lesson."
+    else
+      redirect_to courses_path, error: "There was an error processing your request. Please try again later."
+    end
   end
 
   def index
