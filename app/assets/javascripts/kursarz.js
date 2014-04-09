ef3ed7c04a547ade78d26d286a7eec026875f6ed
Kaminski91 <kaminski91@gmail.com>
2014-04-09 22:59:46 +0200

    Create lesson form. JS page load.

--- app/assets/javascripts/kursarz.js
+++ app/assets/javascripts/kursarz.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+var ready = function() {
 	// Dropdown menu on hover
 	$('.dropdown').mouseenter(function() {
 		$(this).addClass("open");
@@ -8,4 +8,7 @@ $(document).ready(function () {
 	// Dropdown menu min-width;
 	var $dmenu = $('.dropdown-menu')
 	$dmenu.css('min-width', $dmenu.prev().outerWidth() + 'px');
-});
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
