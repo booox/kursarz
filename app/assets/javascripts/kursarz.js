1a7a069820f3a43b943484daa5abfb36cf996612
Kaminski91 <kaminski91@gmail.com>
2014-04-10 15:39:04 +0200

    Youtube move resize. Delete lesson button (not working but exists)

--- app/assets/javascripts/kursarz.js
+++ app/assets/javascripts/kursarz.js
@@ -1,3 +1,15 @@
+var ytresize = function() {
+	var pW = $('#main-page').width();
+	$('iframe').load().each(function() {
+		var iW = $(this).attr('width');
+		var iH = $(this).attr('height');
+		$(this).attr({
+			'width': pW,
+			'height': pW * iH / iW
+		});
+	});
+}
+
 var ready = function() {
 	// Dropdown menu on hover
 	$('.dropdown').mouseenter(function() {
@@ -8,7 +20,15 @@ var ready = function() {
 	// Dropdown menu min-width;
 	var $dmenu = $('.dropdown-menu')
 	$dmenu.css('min-width', $dmenu.prev().outerWidth() + 'px');
+
+	// YT resize
+	ytresize();
 };
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
+// window.resize
+$(window).resize(function() {
+	ytresize();
+});
