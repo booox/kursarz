a70d8a3c623273d116524ac2b95ae5cd8ecd8f97
Michał Darda <michaldarda@gmail.com>
2014-04-18 17:04:54 +0200

    Placeholders for assignments and quizees

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -4,4 +4,8 @@ class Assignment < ActiveRecord::Base
   belongs_to :course
 
   has_many :assignment_submissions
+
+  def screenshot_url
+    "holder.js/750x563/auto/sky/text:#{name}"
+  end
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,8 @@ class Quiz < ActiveRecord::Base
   has_many :questions
 
   accepts_nested_attributes_for :questions
+
+  def screenshot_url
+    super || "holder.js/750x563/auto/sky/text:#{name}"
+  end
 end
