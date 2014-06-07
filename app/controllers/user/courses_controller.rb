67e91b751fed8e5f2c6b223710039868753608ef
Michał Darda <michaldarda@gmail.com>
2014-06-07 18:05:35 +0200

    Teached and owned courses list

--- app/controllers/user/courses_controller.rb
+++ app/controllers/user/courses_controller.rb
@@ -1,5 +1,7 @@
 class User::CoursesController < ApplicationController
   def index
     @courses = current_user.signup_courses
+    @owned_courses = current_user.courses
+    @teached_courses = current_user.teached_courses
   end
 end
