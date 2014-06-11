c0169e9af6edef156b15218ce865800d51dd6a0b
Kaminski91 <kaminski91@gmail.com>
2014-06-12 01:26:00 +0200

    Fix UI #2 View/Hide

--- app/assets/javascripts/course.js
+++ app/assets/javascripts/course.js
@@ -3,8 +3,14 @@ $(document).ready(function() {
 	if(isSignedUpForCourse) {
 		$('#show_description').click(function(e) {
 			e.preventDefault();
-			$('#course_description').animate({height:"toggle"});
-			console.log( $('#course_description').css('display') );
+			$('#course_description').animate({height:"toggle"},function(){
+				if( $('#course_description').css('display') == 'none' ) {
+					$('#show_description').html('View details');
+				} else {
+					$('#show_description').html('Hide details');
+				}
+			});
+			
 		});
 	} else {
 		$('#course_description').show();
