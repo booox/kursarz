cab981ff05f7f72b2d86895247033731731fdebf
Kaminski91 <kaminski91@gmail.com>
2014-06-15 00:38:25 +0200

    Main page v2

--- app/assets/javascripts/kursarz_home.js
+++ app/assets/javascripts/kursarz_home.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
 	// Main page background scale
 	skaluj($(window), $('body'), 3264, 2448);
+
+	// Slide to courselist
+	$('#toCourses').click(function(e) {
+		e.preventDefault();
+		$('html, body').animate({
+		    scrollTop: $("#courses").offset().top
+		 }, 900);
+	});
 });
 
 $(window).resize(function() {
