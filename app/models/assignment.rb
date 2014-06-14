29e66989c4b5fe1f3b10be9862acf37182c8322a
Michał Darda <michaldarda@gmail.com>
2014-06-14 20:05:53 +0200

    Missing validations

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,11 @@
 class Assignment < ActiveRecord::Base
   LANGUAGES = ["ruby", "javascript", "python", "coffeescript"].freeze
 
+  validates :name, presence: true
+  validates :short_description, presence: true
+  validates :description, presence: true
+  validates :code, presence: true
+
   belongs_to :course
   belongs_to :user
 
