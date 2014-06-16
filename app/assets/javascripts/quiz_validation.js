3bb9bd516b9b273d6472219805a7d89f387848f7
Kaminski91 <kaminski91@gmail.com>
2014-06-16 17:28:30 +0200

    Quiz validation

--- app/assets/javascripts/quiz_validation.js
+++ app/assets/javascripts/quiz_validation.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 		// Div na błędy
 		var error = $('<div />').addClass('alert alert-danger');
 
+		// Inputy
 		$('#new_quiz').find('input').not('[type="checkbox"]').each(function() {
 			if( $(this).val() == "" ) {
 				empyInput = true;
@@ -23,16 +24,37 @@ $(document).ready(function() {
 					errMsg += 'Answer ';
 				} else if( $(this).attr('name') == "quiz[name]" ) {
 					errMsg += 'Name ';
-				} else if( $(this).attr('name') == "quiz[short_description]" ) {
+				}
+
+				errMsg += "can't be blank";
+				errMsgObj.html( errMsg ); 
+				error.append( errMsgObj );
+			}
+		});
+
+		// Textarea
+		$('#new_quiz').find('textarea').each(function() {
+
+			if( $(this).val() == "" ) {
+				empyInput = true;
+				$(this).parent().addClass('has-error');
+
+				// Append error msg
+				var errMsgObj = $('<p />');
+				var errMsg = '';
+				if( $(this).attr('name') == "quiz[short_description]" ) {
 					errMsg += 'Short description ';
 				} else if( $(this).attr('name') == "quiz[description]" ) {
 					errMsg += 'Description ';
 				}
 
 				errMsg += "can't be blank";
+				errMsgObj.html( errMsg ); 
 				error.append( errMsgObj );
 			}
+
 		});
+
 		if( !empyInput ) {
 			$('#new_quiz').submit();
 		} else {
