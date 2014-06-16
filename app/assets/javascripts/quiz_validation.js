69d0004ce9339899fcbb48998a0ed94ce45fe14e
Kaminski91 <kaminski91@gmail.com>
2014-06-16 14:42:58 +0200

    Quiz js validation

---
+++ app/assets/javascripts/quiz_validation.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+	$('input[type="submit"]').click(function(e) {
+		e.preventDefault();
+
+		// Flaga putych inputów
+		var empyInput = false;
+
+		// Div na błędy
+		var error = $('<div />').addClass('alert alert-danger');
+
+		$('#new_quiz').find('input').not('[type="checkbox"]').each(function() {
+			if( $(this).val() == "" ) {
+				empyInput = true;
+				$(this).parent().addClass('has-error');
+
+				// Append error msg
+				var errMsgObj = $('<p />');
+				var errMsg = '';
+				if( $(this).hasClass('question-input') ) {
+					errMsg += 'Question ';
+				} else if( $(this).hasClass('answer-input') ) {
+					errMsg += 'Answer ';
+				} else if( $(this).attr('name') == "quiz[name]" ) {
+					errMsg += 'Name ';
+				} else if( $(this).attr('name') == "quiz[short_description]" ) {
+					errMsg += 'Short description ';
+				} else if( $(this).attr('name') == "quiz[description]" ) {
+					errMsg += 'Description ';
+				}
+
+				errMsg += "can't be blank";
+				error.append( errMsgObj );
+			}
+		});
+		if( !empyInput ) {
+			$('#new_quiz').submit();
+		} else {
+			$('.alert-warning').after( error );
+		}
+	});
+
+});
