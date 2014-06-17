4453b9bcdc207a59922ff00c02b973fef875fef8
Michał Darda <michaldarda@gmail.com>
2014-06-17 18:32:05 +0200

    Production github and bitbucket auth keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '5d5c6f51ec961af04433', '44e68aa54d21e32781bd8e5c8d236220d936f21b'
-  provider :bitbucket, 'WWyhYsstfN6VdYjbnf', 'zYKh7AJJ7n8JjagKWr8y48pjtxh4Z8Uw'
+  provider :github, Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret
+  provider :bitbucket, Rails.application.secrets.bitbucket_client_id, Rails.application.secrets.bitbucket_client_secret
 end
